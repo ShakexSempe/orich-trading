15384f768f11421531a6b5e394270f27ba0d7a3a
Ray-Shakex <bshakex@gmail.com>
2022-01-27 11:55:30 +0200

    test: js: background scroll bg color

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const toggle = document.getElementById("toggle");
+const header = document.getElementById("header");
 const main = document.getElementById("main");
 const aside = document.getElementById("aside");
 const links = document.querySelectorAll('#aside ul li a');
-console.log(links);
+console.log(header);
+
+document.addEventListener("scroll", () => {
+    const scroll_position = window.scrollY;
+    const scroll_height = window.pageYOffset;
+
+    if(scroll_position < 150) {
+        header.classList.remove("scroll");
+    } else {
+        header.classList.add("scroll");
+    }
+});
 
 toggle.addEventListener("click",  () => {
     aside.classList.toggle("active");
     toggle.classList.toggle("active");
     main.classList.toggle("active");
+    header.classList.add("scroll");
 });
 
 links.forEach(link => {
@@ -15,6 +28,7 @@ links.forEach(link => {
         aside.classList.remove("active");
         toggle.classList.remove("active");
         main.classList.remove("active");
+        header.classList.add("scroll");
     });
 });
 
