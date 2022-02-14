e6d7a6097fd5aef09380fc57b133c3ff16ffaa16
Ray-Shakex <bshakex@gmail.com>
2022-02-14 13:17:29 +0200

    app.js bg changer

--- js/app.js
+++ js/app.js
@@ -68,8 +68,8 @@ main.addEventListener("click", () => {
 });
 
 
-// bgChanger.forEach(changer => {
-//     changer.addEventListener("click", () => {
-//         hero.classList.toggle("active");
-//     })
-// })
+bgChanger.forEach(changer => {
+    changer.addEventListener("click", () => {
+        hero.classList.toggle("active");
+    })
+})
