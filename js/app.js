fa00d7c8842e832cb8608eae6c109d815a7c067a
Ray-Shakex <bshakex@gmail.com>
2022-02-15 09:21:31 +0200

    document eventlistener scroll position

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ document.addEventListener("scroll", () => {
     const scroll_position = window.scrollY;
     const scroll_height = window.pageYOffset;
 
-    if(scroll_position < 30) {
+    if(scroll_position < 25) {
         header.classList.remove("scroll");
         logo.style.opacity = 0;
         
