288cfe9308b176b9c9bf0d3d0df71543ea6ebb61
Ray-Shakex <bshakex@gmail.com>
2022-04-07 12:16:10 +0200

    aside social links

--- js/app.js
+++ js/app.js
@@ -89,33 +89,3 @@ main.addEventListener("click", () => {
 
 // pageMainObserver.observe(pageMain);
 
-// INTERSECTION OBSERVER
-const indexHeader = document.querySelector(".index-header");
-
-const heroOptions = {
-    rootMargin: "-90% 0px 0px 0px"
-};
-
-const heroObserver = new IntersectionObserver(
-    function(
-        entries, heroObserver
-    ) {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting) {
-                console.log("hero NOT intersecting");
-                indexHeader.classList.add("observe");
-                topBtn.classList.add("active");
-                logo.classList.remove("active-logo");
-            } else {
-                console.log("hero intersecting");
-                indexHeader.classList.remove("observe");
-                topBtn.classList.remove("active");
-                logo.classList.add("active-logo");
-
-
-            }
-        });
-    }, heroOptions
-);
-
-heroObserver.observe(hero);
