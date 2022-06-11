86b277983a587c979b0304e201a66e0f1c2b2c65
Ray-Shakex <bshakex@gmail.com>
2022-06-11 12:19:03 +0200

    toggle = add .toggle-header

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ toggle.addEventListener("click",  () => {
     header.classList.remove("scroll");
     header.classList.remove("hero-header");
     header.classList.toggle("toggle-header");
+    header.classList.toggle("main-header");
     console.log("open");
     // logo.classList.remove("active-logo");
 });
